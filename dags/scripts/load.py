20a225a925e7380411c8bf5c4d07f886be9ea592
shoaibswe <shoaibswe@gmail.com>
2023-10-27 21:14:54 +0600

    update: table creation query

--- dags/scripts/load.py
+++ dags/scripts/load.py
@@ -2,14 +2,21 @@ import os
 import csv
 
 from model import Connection, Users, Locations, Additional
-import dags.scripts.config as config
+# import dags.scripts.config as config
+import sys
+sys.path.insert(0,'/Users/shuvo/Documents/doc/Take-Home-Assignment-main/dags/')
+import scripts.config as config
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
 
 def get_file_path():
     # should return a os file path with correct destination.
     # Do not change file name
     # write your code here
     filename = "random_user.csv"
-    filepath = ''
+    filepath = os.path.join(os.getcwd(), filename)
     return filepath
 
 def main():
@@ -20,8 +27,19 @@ def main():
     # Create users, locations, additinal object
     # insert these object in the array and then into our data warehouse
     with open(filename, encoding='utf-8') as csvf:
-        for row in csv_reader:
-            pass
+        csv_reader = csv.DictReader(csvf)
+        header_row = csv_reader.fieldnames
+        print(header_row)
+
+        # for row in csv_reader:
+        #     user = Users(name=row['name'])
+        #     location = Locations(city=row['location_city'], state=row['location_state'])
+        #     additional = Additional(email=row['email'], phone=row['phone'])
+        #     user.location = location
+        #     user.additional = additional
+        #     data_insert.append(user)
+
+            # pass
             #user = Users(name=row['name'])
 
     # Connect with the db
@@ -31,6 +49,20 @@ def main():
     # commit db
     # close db
     # write your code here
+    engine = create_engine(config.DB_CONNECTION_STRING_WAREHOUSE)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        session.query(Users).delete()
+        session.bulk_save_objects(data_insert)
+        session.commit()
+        print("Data loaded successfully.")
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        session.rollback()
+    finally:
+        session.close()
 
 if __name__ == '__main__':
     main()
