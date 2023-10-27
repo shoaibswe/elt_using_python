888b6f5b897b8136dd7374a64bf5f295b51ecd9e
shoaibswe <shoaibswe@gmail.com>
2023-10-27 18:13:32 +0600

    Update: updated migration and model py files

--- dags/scripts/migration.py
+++ dags/scripts/migration.py
@@ -2,8 +2,13 @@ import argparse
 from pathlib import Path
 
 from model import Connection
-import dags.scripts.config as config
+# import dags.scripts.config as config
+import sys
+sys.path.insert(0,'/Users/shuvo/Documents/doc/Take-Home-Assignment-main/dags/')
+import scripts.config as config
 
+from sqlalchemy import create_engine,text
+import os
 
 def init_csv_file():
     Path(config.CSV_FILE_DIR).mkdir(parents=True, exist_ok=True)
@@ -11,14 +16,41 @@ def init_csv_file():
 # Initialize schema and table
 def init_db():
     # Stablish a db connection
+    engine= create_engine(config.DB_CONNECTION_STRING_WAREHOUSE)
     # get db session
+    
+    # connection= Connection(engine) 
+    # session = connection.session()
     # create a schema named raw
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    schema_file_path = os.path.abspath(os.path.join(script_dir,'..','..','scripts','schema.sql'))
+    table_file_path = os.path.abspath(os.path.join(script_dir,'..','..','scripts','table.sql'))
+   
+    print("Script Dir is: ",script_dir)
+    print(schema_file_path)
+    print(table_file_path)
+    connection = Connection(engine)
+    session = connection.get_session()
+
+    with open(schema_file_path,'r') as schema_file:
+        schema_commands = text(schema_file.read())
+        session.execute(schema_commands)
+
+    with open(table_file_path,'r') as table_file:
+        tbl_commands = text(table_file.read())
+        session.execute(tbl_commands)
+    
     # create users, locations, additional table in schema raw with appropiate columns
     # commit db
     # close db
-    pass
+
+    session.commit()
+    session.close()
+    # pass
 
 
 if __name__ == '__main__':
     init_csv_file()
     init_db()
+
+#INSTALLED SQLALCHEMY
--- dags/scripts/model.py
+++ dags/scripts/model.py
@@ -4,13 +4,15 @@ import uuid
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import create_engine, Column
+from sqlalchemy import create_engine, Column, String
 
 
 class Connection(object):
-    def __init__(self):
-        engine = create_engine(config.DB_CONNECTION_STRING_WAREHOUSE)
-        self.engine = engine
+    def __init__(self,engine=None):
+        if engine is not None:
+            self.engine = engine
+        else:
+            self.engine = create_engine(config.DB_CONNECTION_STRING_WAREHOUSE)
 
     def get_session(self):
         Session = sessionmaker(bind=self.engine)
@@ -33,26 +35,55 @@ class Users(Base):
 
 	# define required database columes
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    gender = Column(String)
+    name = Column(String)
+    first = Column(String)
+    last = Column(String)
+    date_of_birth = Column(String)
 
-    def __init__(self, name, gender):
+    def __init__(self, gender,name,first, last, date_of_birth):
+        self.gender = gender
+        self.name = name
+        self.name = name
+        self.last = last
+        self.date_of_birth = date_of_birth
         # init database model
-        pass
+        # pass
 
 
 class Locations(Base):
     __tablename__ = 'locations'
 
 	# define required database columes
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    city = Column(String)
+    state = Column(String)
+    country = Column(String)
+    postcode = Column(String)
+    country_code = Column(String) 
 
-    def __init__(self, state):
+    def __init__(self, city, state, country, postcode, country_code):
         # init database model
-        pass
+        self.city = city
+        self.state = state
+        self.country = country
+        self.postcode = postcode
+        self.country_code = country_code
+
+        # pass
 
 class Additional(Base):
     __tablename__ = 'additional'
 
 	# define required database columes
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    phone = Column(String)
+    email = Column(String)
+    picture_large = Column(String) 
 
-    def __init__(self, phone):
+    def __init__(self, phone,email, picture_large):
         # init database model
-        pass
+        self.phone = phone
+        self.email=email
+        self.picture_large=picture_large
+        # pass
